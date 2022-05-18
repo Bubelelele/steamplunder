89baf8ad2d48827cf6ced633cfd82ea50c9b701e
Bubelelele <89357128+Bubelelele@users.noreply.github.com>
2022-05-18 19:17:12 +0200

    Revert "Merge branch 'main' of https://github.com/Bubelelele/steamplunder"
    This reverts commit 17a0d598a0827f5b116e1def23c3248cd3ec9916, reversing
    changes made to 02cc04def9e2f713fc037f25cc534e9131caad58.

--- Assets/_Scripts/Puzzle Elements/Rotating Symbols/RotatingSymbolsPuzzleController.cs
+++ Assets/_Scripts/Puzzle Elements/Rotating Symbols/RotatingSymbolsPuzzleController.cs
@@ -3,37 +3,28 @@ using UnityEngine.Events;
 
 public class RotatingSymbolsPuzzleController : MonoBehaviour {
 
-    [SerializeField] private RotatingSymbolPedestal[] symbolPedestals;
+    [SerializeField] private int[] solution;
+    [SerializeField] private RotatingSymbolBlock[] blocks;
     [SerializeField] private UnityEvent onPuzzleCompletion;
     
     private bool _solved;
 
     private void Start() {
-        foreach (var pedestal in symbolPedestals) {
-            pedestal.onCorrect.AddListener(CheckSolution);
-        }
+        if (solution.Length != blocks.Length) Debug.LogWarning("Rotating block puzzle solution length error");
 
-        while (AllPedestalsSolved()) {
-            //will scramble all pedestals until it is not a completed state
-            Debug.Log("Scrambling");
-            foreach (var pedestal in symbolPedestals) {
-                pedestal.Scramble();
-            }
+        foreach (var block in blocks) {
+            block.OnRotate += CheckSolution;
         }
     }
 
     private void CheckSolution() {
-        if (_solved || !AllPedestalsSolved()) return;
+        if (_solved) return;
+        
+        for (int i = 0; i < blocks.Length; i++) {
+            if (blocks[i].CurrentSymbol != solution[i]) return;
+        }
 
-        //Symbols are correctly rotated if this point is reached
         _solved = true;
         onPuzzleCompletion.Invoke();
-    }
-    
-    private bool AllPedestalsSolved() {
-        foreach (var pedestal in symbolPedestals) {
-            if (!pedestal.Solved) return false;
-        }
-        return true;
     }
 }
