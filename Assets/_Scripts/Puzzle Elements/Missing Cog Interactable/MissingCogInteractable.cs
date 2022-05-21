0fe9b562b4ba18cc76b57e09dbb01448ea6d275f
tusjmod <64898919+tusjmod@users.noreply.github.com>
2022-05-21 21:19:39 +0200

    missing cog interactable

---
+++ Assets/_Scripts/Puzzle Elements/Missing Cog Interactable/MissingCogInteractable.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MissingCogInteractable : MonoBehaviour, IInteractable {
+    
+    [SerializeField] private UnityEvent onCogPlaced;
+    [SerializeField] private GameObject cogCompleted;
+    [SerializeField] private GameObject cogIndicator;
+
+    private bool _cogPickedUp;
+    private Animator _animator;
+
+    private void Awake() {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void CogPickedUp() {
+        _cogPickedUp = true;
+        cogIndicator.SetActive(true);
+    }
+
+    public bool HoldToInteract { get; }
+    public void Interact() {
+        if (!_cogPickedUp) return;
+        
+        _animator.SetTrigger("Completed");
+        cogCompleted.SetActive(true);
+        cogIndicator.SetActive(false);
+        onCogPlaced.Invoke();
+        Destroy(this);
+    }
+
+    public string GetDescription() => _cogPickedUp ? "Place Cog" : "(Cog Missing)";
+
+    public string GetKeyText() => null;
+}
