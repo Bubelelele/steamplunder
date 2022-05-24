c195004a100e180d8e4ddfd6be7100b66a99a488
sort7er <98805454+sort7er@users.noreply.github.com>
2022-05-24 14:31:20 +0200

    WeaponTrigger

--- Assets/_Scripts/Entity/Enemies/Enemy Components/WeaponTrigger.cs
+++ Assets/_Scripts/Entity/Enemies/Enemy Components/WeaponTrigger.cs
@@ -6,14 +6,31 @@ public class WeaponTrigger : MonoBehaviour {
     public GameObject enemy;
 
     private MeleeBandit _meleeBandit;
+    private HeavyBandit _heavyBandit;
+
+    private bool _isMelee;
+    private bool _isHeavy;
 
     private void Awake() {
-        _meleeBandit = enemy.GetComponent<MeleeBandit>();
+        if(enemy.GetComponent<MeleeBandit>() != null)
+        {
+            _meleeBandit = enemy.GetComponent<MeleeBandit>();
+            _isMelee = true;
+        }
+        else if(enemy.GetComponent<HeavyBandit>() != null)
+        {
+            _heavyBandit = enemy.GetComponent<HeavyBandit>();
+            _isHeavy = true;
+        }  
     }
 
     private void OnTriggerEnter(Collider other) {
-        if (_meleeBandit.lethal && other.CompareTag("Player")) {
+        if (_isMelee && _meleeBandit.lethal && other.CompareTag("Player")) {
             PlayerData.Damage(_meleeBandit.attackDamage, transform);
+        }
+        if (_isHeavy && _heavyBandit.lethal && other.CompareTag("Player"))
+        {
+            PlayerData.Damage(_heavyBandit.attackDamage, transform);
         }
     }
 }
