bfb842aff52645a8a6a2fd9ff3cdebe1b4581694
tusjmod <64898919+tusjmod@users.noreply.github.com>
2022-06-04 17:13:05 +0200

    note pedestal also give cog

--- Assets/_Scripts/Tools/CogSpawn.cs
+++ Assets/_Scripts/Tools/CogSpawn.cs
@@ -1,12 +1,20 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class CogSpawn : MonoBehaviour {
     
     [SerializeField] private int amountOfCogs;
+    [SerializeField] private bool spawnOnAwake;
 
-    public void SpawnCogs() {
+    private void Awake() {
+        if (spawnOnAwake) StartCoroutine(SpawnCogs());
+    }
+
+    public IEnumerator SpawnCogs() {
         for (int i = 0; i < amountOfCogs; i++) {
             EffectSpawner.SpawnDroppedCog(transform.position);
+            yield return new WaitForSeconds(.4f);
         }
     }
 
