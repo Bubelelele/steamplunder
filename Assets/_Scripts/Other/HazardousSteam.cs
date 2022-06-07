856663568c3e952c184235b885b7044d51c7a6cd
tusjmod <64898919+tusjmod@users.noreply.github.com>
2022-06-07 22:17:21 +0200

    damage steam update

--- Assets/_Scripts/Other/HazardousSteam.cs
+++ Assets/_Scripts/Other/HazardousSteam.cs
@@ -9,17 +9,21 @@ public class HazardousSteam : Campfire {
     [SerializeField] private float cooldownTime = 4f;
 
     private VisualEffect _vfx;
+    private Collider _triggerCollider;
 
     private void Start() {
         _vfx = GetComponent<VisualEffect>();
+        _triggerCollider = GetComponent<Collider>();
         if (_vfx == null) return;
         StartCoroutine(HandleSteamFlow());
     }
 
     private IEnumerator HandleSteamFlow() {
         _vfx.Play();
+        _triggerCollider.enabled = true;
         yield return new WaitForSeconds(activeTime);
         _vfx.Stop();
+        _triggerCollider.enabled = false;
         yield return new WaitForSeconds(cooldownTime);
         StartCoroutine(HandleSteamFlow());
     }
