8f9478b9937d7552c640e67ba150a1b44d6518cf
tusjmod <64898919+tusjmod@users.noreply.github.com>
2022-06-07 22:55:05 +0200

    Update CutscenePlayer.cs

--- Assets/_Scripts/Tools/CutscenePlayer.cs
+++ Assets/_Scripts/Tools/CutscenePlayer.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Playables;
 
 public class CutscenePlayer : MonoBehaviour {
     
     [SerializeField] private bool canPlayMultipleTimes;
     [SerializeField] private PlayableDirector playableDirector;
+    [SerializeField] private UnityEvent onCutsceneFinished;
 
     private bool _played;
 
@@ -12,7 +14,14 @@ public class CutscenePlayer : MonoBehaviour {
         if (!canPlayMultipleTimes && _played) return;
 
         _played = true;
+        CutsceneManager.OnCutscenePlaying += OnCutscenePlaying;
         CutsceneManager.PlayCutscene(playableDirector);
+    }
+    
+    private void OnCutscenePlaying(bool isPlaying) {
+        if (isPlaying) return;
+        CutsceneManager.OnCutscenePlaying -= OnCutscenePlaying;
+        onCutsceneFinished.Invoke();
     }
 
 }
