fa801ab4bf2aa2b2bd0daa35484ed8c6dd5ed5a2
tusjmod <64898919+tusjmod@users.noreply.github.com>
2022-06-07 17:57:59 +0200

    roundabout stuff lol + sound on it

--- Assets/_Scripts/Interactable/Roundabout.cs
+++ Assets/_Scripts/Interactable/Roundabout.cs
@@ -5,11 +5,14 @@ public class Roundabout : MonoBehaviour {
     [SerializeField] private float rotationSpeed = 10f;
     [SerializeField] private Transform bridge;
     [SerializeField] private Transform backCog;
-
+    [SerializeField] private AudioSource audioSource;
+    
     public void RotateTick() {
         var rotationAmount = rotationSpeed * Time.deltaTime;
         bridge.Rotate(-rotationAmount * Vector3.up);
         backCog.Rotate(rotationAmount * Vector3.forward);
+        
+        if (!audioSource.isPlaying) audioSource.Play();
     }
     
 }
