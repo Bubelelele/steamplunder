f57079a9b773600fb61c130a08e04e6fe1e25508
tusjmod <64898919+tusjmod@users.noreply.github.com>
2022-06-09 02:06:41 +0200

    hazard steam sound

--- Assets/_Scripts/Other/HazardousSteam.cs
+++ Assets/_Scripts/Other/HazardousSteam.cs
@@ -9,19 +9,23 @@ public class HazardousSteam : Campfire {
 
     private VisualEffect _vfx;
     private Collider _triggerCollider;
+    private AudioSource _audioSource;
 
     private void Start() {
         _vfx = GetComponent<VisualEffect>();
         _triggerCollider = GetComponent<Collider>();
+        _audioSource = GetComponent<AudioSource>();
         if (_vfx == null) return;
         StartCoroutine(HandleSteamFlow());
     }
 
     private IEnumerator HandleSteamFlow() {
         _vfx.Play();
+        _audioSource.Play();
         _triggerCollider.enabled = true;
         yield return new WaitForSeconds(activeTime);
         _vfx.Stop();
+        _audioSource.Stop();
         _triggerCollider.enabled = false;
         yield return new WaitForSeconds(cooldownTime);
         StartCoroutine(HandleSteamFlow());
