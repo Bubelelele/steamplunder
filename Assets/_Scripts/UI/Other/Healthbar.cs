9715ffca0305146caf7cadc5eefcae4ad4a3bde1
tusjmod <64898919+tusjmod@users.noreply.github.com>
2022-06-09 07:28:52 +0200

    healthbar update

--- Assets/_Scripts/UI/Other/Healthbar.cs
+++ Assets/_Scripts/UI/Other/Healthbar.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class Healthbar : MonoBehaviour {
@@ -7,6 +8,7 @@ public class Healthbar : MonoBehaviour {
     [SerializeField] private Image healthbarImage;
     [SerializeField] private float reduceSpeed = 2f;
     [SerializeField] private TMP_Text healthText;
+    [SerializeField] private UnityEvent onHealthbarEmpty;
     
     private float _target = 1;
     
@@ -18,5 +20,8 @@ public class Healthbar : MonoBehaviour {
 
     private void Update() {
         healthbarImage.fillAmount = Mathf.MoveTowards(healthbarImage.fillAmount, _target, reduceSpeed * Time.deltaTime);
+        if (healthbarImage.fillAmount == 0f) {
+            onHealthbarEmpty.Invoke();
+        }
     }
 }
