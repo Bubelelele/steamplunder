9438d24d164410c2b6f06df5800b5cbc695cc5c9
tusjmod <64898919+tusjmod@users.noreply.github.com>
2022-06-09 10:30:15 +0200

    grapple contraption line

---
+++ Assets/_Scripts/Other/LineRendererUpdater.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class LineRendererUpdater : MonoBehaviour {
+
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private Transform transform1;
+    [SerializeField] private Transform transform2;
+
+    private void Update() {
+        DrawRope();
+    }
+
+    private void DrawRope() {
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(0, transform1.position);
+        lineRenderer.SetPosition(1, transform2.position);
+    }
+
+}
